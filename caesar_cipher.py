b8bfd3ba73e21c70aa3f198ce87b3f3cb244af05
Jay Lao <cjaylao447@gmail.com>
2026-01-26 16:16:31 +0800

    feat: caesar cipher

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -1,16 +1,24 @@
 from encryption import encrypt
 from decryption import decrypt
 
-print("Caesar Cipher Program")
-print("1. Encrypt")
-print("2. Decrypt")
+while True:
+    print("\n=== Caesar Cipher Program ===")
+    print("1. Encrypt")
+    print("2. Decrypt")
+    print("3. Exit")
 
-choice = input("Choose an option (1 or 2): ")
-message = input("Enter your message: ")
+    choice = input("Choose an option (1, 2, 3): ")
 
-if choice == "1":
-    print("Encrypted message:", encrypt(message))
-elif choice == "2":
-    print("Decrypted message:", decrypt(message))
-else:
-    print("Invalid choice")
+    if choice == "3":
+        print("Goodbye!")
+        break
+
+    message = input("Enter your message: ")
+    shift = int(input("Enter shift key: "))
+
+    if choice == "1":
+        print("Encrypted message:", encrypt(message, shift))
+    elif choice == "2":
+        print("Decrypted message:", decrypt(message, shift))
+    else:
+        print("Invalid choice. Please try again.")
